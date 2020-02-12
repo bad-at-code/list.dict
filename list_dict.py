013164e2587ccca17c66b7572f1275a5b6f7e447
jka2004 <josh.k.abraham@gmail.com>
2020-02-12 11:32:19 -0600

    Did the thing

---
+++ list_dict.py
@@ -0,0 +1,24 @@
+"""hello."""
+print("Dictionary")
+dict = {1: 'Example', 2: 'Example2', 3: 'Example3'}
+print(dict)
+
+print()
+print()
+
+print("Appending the Dictionary")
+dict[4] = 'Example4'
+print(dict)
+
+print()
+print()
+
+print("Finding a part of the Dictionary")
+print(dict[1])
+
+print()
+print()
+
+print("Deleting a part of a Dictionary")
+del dict[2]
+print(dict)
